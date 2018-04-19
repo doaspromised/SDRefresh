8eddca5a87a4551c30b14a6589b9e6b48dadeced
姜守栋 <18511632756@163.com>
2018-04-19 11:22:03 +0800

    根据拖动状态以及拖动的距离进行组合，修改控件当前的状态

--- Refresh/Refresh/Refresh/RefreshHeader.swift
+++ Refresh/Refresh/Refresh/RefreshHeader.swift
@@ -7,15 +7,15 @@
 //
 
 import UIKit
-
+fileprivate let headerHeight: CGFloat = 64.0
 class RefreshHeader: RefreshBaseView {
     
-//    override var state: RefreshState {
-//        
-//        didSet {
-//            
-//        }
-//    }
+    override var state: RefreshState {
+        
+        didSet {
+            print(state)
+        }
+    }
     
     static func header(with refreshingBlcok: @escaping refreshBlcok ) -> RefreshHeader {
         let header = self.init()
@@ -26,12 +26,31 @@ class RefreshHeader: RefreshBaseView {
     
     override func placeSubviews() {
         sd_x = 0.0
-        sd_height = 64.0
+        sd_height = headerHeight
         sd_width = (scrollView?.sd_width)!
         sd_y = -sd_height
     }
     
     override func scrollViewContentOffsetChanged(_ value: [NSKeyValueChangeKey : Any]?) {
+        
+        guard let scrollView = scrollView else {
+            return
+        }
+        if scrollView.isDragging {
+            
+            if state == .idle && scrollView.sd_offsetY < -headerHeight {
+                state = .pulling
+            } else if state == .pulling && scrollView.sd_offsetY > -headerHeight {
+                state = .idle
+            }
+            
+        } else {
+            
+            if state == .pulling && scrollView.sd_offsetY <= -headerHeight {
+                state = .refreshing
+            }
+            
+        }
         
     }
     
